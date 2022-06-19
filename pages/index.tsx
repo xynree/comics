c3a169becab3e79cf0b4f0d20fdefdfd877a0b8c
Xinrui Chen <xynree@gmail.com>
2022-06-19 15:16:09 -0700

    [UPDATE] adjust favicon and title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
 import comics_sorter from '../helpers/comics_sorter'
 import Main from '../components/Main';
 import client from '../cms/client';
-
+import Head from 'next/head'
 
 export default function index({ comics }) {
   return (
+    <>
+    <Head>
+      <title>xyn's diary</title>
+      <link rel="icon" href="favicon.png" />
+    </Head>
     <Main comics={comics}/>
+    </>
   );
 }
 
