2131763299bd4afa4c4ab13248056aa2b004694c
Xinrui Chen <xynree@gmail.com>
2022-06-19 23:00:19 -0700

    [UPDATE] Adjust sizing on mobile

--- components/ComicViewer/index.tsx
+++ components/ComicViewer/index.tsx
@@ -6,9 +6,9 @@ const { container, comicTitle, comicView, comicDesc, comicStyle} = {
   container:
     "flex h-full pt-10 pb-12 flex-col justify-between gap-6 ",
   comicTitle: "text-3xl text-center [letter-spacing:25px]",
-  comicView: "w-4/5 h-auto xl:w-3/5  m-auto flex flex-col justify-start items-center gap-4 p-2",
+  comicView: "w-full md:w-4/5 h-auto xl:w-3/5  m-auto flex flex-col justify-start items-center gap-4 ",
   comicDesc: "text-center font-nw-thin text-sm",
-  comicStyle: "shadow-xl w-full h-auto rounded-lg",
+  comicStyle: "shadow-xl w-full h-auto md:rounded-lg",
 };
 
 const ComicViewer = ({ comic, navigate }) => {
