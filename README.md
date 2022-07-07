8f5baee9f8ef446f1b09b2ad65166197db38d4dc
Xinrui Chen <xynree@gmail.com>
2022-07-07 14:30:22 -0700

    [ADD]  Update README

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # Diary Comics
 
 ## About
-Site to host my ongoing diary comics making practice. Written in NEXT.js and Typescript using Sanity's headless CMS.
+This is a personal site to host my ongoing diary comics making practice.
+
+## Usage
+Run `npm run dev` to view locally.
+Unit tests can be run with `npm run test` and integration tests with `npm run e2e`.
+
+View the deployed version of this project [here](https://comics.xinrui.work/).
+
+
+## Technologies Used
+Written in `NEXT.js` and `Javascript`/`Typescript` using `Sanity` as a headless CMS.
+Testing is set up with `Jest`, `Playwright`, and `React-Testing-Library`.
---
+++ __tests__/comics_sorter.test.tsx
@@ -0,0 +1,250 @@
+import comics_sorter from "../helpers/comics_sorter";
+import { Comic } from "../types/types";
+
+const one: any = {
+  _createdAt: "2022-06-18T04:22:40Z",
+  _id: "b933bda1b379d0c910868825e96f7ead9060218120250bfaf25287bf47659b28",
+  _rev: "sGrqvfP2mxJ5kXjUs6W3o3",
+  _type: "comic",
+  _updatedAt: "2022-06-18T04:28:04Z",
+  description: "",
+  images: [
+    {
+      _key: "FOEMXZ2J",
+      _type: "image",
+      asset: {
+        _ref: "image-67a6f740afcf96ad9bf05fbc2695eb3806f886f2-2048x2048-jpg",
+        _type: "reference",
+      },
+    },
+    {
+      _key: "2HKO2Vw3",
+      _type: "image",
+      asset: {
+        _ref: "image-15bec6bdcd3537c9968f3908d7fce22890d6cc21-1440x1440-jpg",
+        _type: "reference",
+      },
+    },
+    {
+      _key: "HyKjD9mU",
+      _type: "image",
+      asset: {
+        _ref: "image-9a654a1d19e14fab77fceea8197309f5ff359b51-1440x1440-jpg",
+        _type: "reference",
+      },
+    },
+    {
+      _key: "IMSheigN",
+      _type: "image",
+      asset: {
+        _ref: "image-38c8d29041004af4c67ab0b89fc747ccbcef0516-1440x1440-jpg",
+        _type: "reference",
+      },
+    },
+  ],
+  title: "8.7.21",
+};
+
+const two: Comic = {
+  _createdAt: "2022-06-18T04:22:40Z",
+  _id: "4e6b1693f4673d0968cbc79417a4429dc7f490836b361161a1473e905ca3fc67",
+  _rev: "sGrqvfP2mxJ5kXjUs6W3o3",
+  _type: "comic",
+  _updatedAt: "2022-06-18T04:28:04Z",
+  description: "",
+  images: [
+    {
+      _key: "5hG6wSwE",
+      _type: "image",
+      asset: {
+        _ref: "image-2244c46224e1212baa63f7c9375d96a1ea744931-2048x2048-png",
+        _type: "reference",
+      },
+    },
+    {
+      _key: "RrFxyTim",
+      _type: "image",
+      asset: {
+        _ref: "image-27b321d6c51d2a2d1398bef920d899bdbf634b11-2048x2048-png",
+        _type: "reference",
+      },
+    },
+    {
+      _key: "q6XMzxOs",
+      _type: "image",
+      asset: {
+        _ref: "image-b8c11a2baf0ef02ecf230dbe5a59de04bcb002bf-2048x2048-png",
+        _type: "reference",
+      },
+    },
+    {
+      _key: "tQLC1elw",
+      _type: "image",
+      asset: {
+        _ref: "image-18c3a5a4f7b4ebef3c181bfc3bce33e996ac7427-2048x2048-png",
+        _type: "reference",
+      },
+    },
+    {
+      _key: "u9uL5kQg",
+      _type: "image",
+      asset: {
+        _ref: "image-d8ccdd5d4cf2ffb9da21f11b0bb5a02bc48dbc77-2048x2048-png",
+        _type: "reference",
+      },
+    },
+    {
+      _key: "zjX2b6KB",
+      _type: "image",
+      asset: {
+        _ref: "image-ee1e91b5a0b41d16e5033fdfdaa579bb5a5f3a7e-2048x2048-png",
+        _type: "reference",
+      },
+    },
+    {
+      _key: "BG55YzdZ",
+      _type: "image",
+      asset: {
+        _ref: "image-f0b1c73e1e4a64c24087a5c87236bd02a1c37a5d-2048x2048-png",
+        _type: "reference",
+      },
+    },
+    {
+      _key: "ZKkMzk69",
+      _type: "image",
+      asset: {
+        _ref: "image-5fd1568f48514b96f7a3c33a8f91b4cd576d889a-2048x2048-png",
+        _type: "reference",
+      },
+    },
+  ],
+  title: "1.29.22",
+};
+
+const three: Comic = {
+  _createdAt: "2022-06-26T03:43:56Z",
+  _id: "863709d5-4193-47dc-b82a-8c2307e98b5a",
+  _rev: "sGrqvfP2mxJ5kXjUsZQBWd",
+  _type: "comic",
+  _updatedAt: "2022-06-26T03:43:56Z",
+  description:
+    "Some light hearted ube product opinions on this honestly sad sad day for America. Actually pretty much all of TJ’s ube stuff has been really good , praying they do not discontinue it as they really often do with a lot of their “Asian” products",
+  images: [
+    {
+      _key: "f69c75ea305c",
+      _type: "image",
+      asset: {
+        _ref: "image-bc70e7eb78d44c65dbce8763e7b08d054f594fdb-2048x2048-png",
+        _type: "reference",
+      },
+    },
+  ],
+  title: "6.24.22",
+};
+
+const four: Comic = {
+  _createdAt: "2022-07-06T18:06:07Z",
+  _id: "e09b8c7f-930a-4191-aab6-fd905fba8052",
+  _rev: "oV5AkreV0DW9Fuq9d9Yfwc",
+  _type: "comic",
+  _updatedAt: "2022-07-07T02:53:24Z",
+  description:
+    'Had a sudden bout of homesickness after eating an avocado. Also i misspelled "loss" to "los"... I miss LOS angeles . But only sometimes',
+  images: [
+    {
+      _key: "ca99e3e0e3f6",
+      _type: "image",
+      asset: {
+        _ref: "image-2d2d8be99aa621e537bea023b89993d2c5fe45e9-2048x2048-png",
+        _type: "reference",
+      },
+    },
+    {
+      _key: "a9c6d99d37f4",
+      _type: "image",
+      asset: {
+        _ref: "image-13c22f59f0d9ea7df09485f8f0d2e20e3064bbcd-2048x2048-png",
+        _type: "reference",
+      },
+    },
+    {
+      _key: "16b4bee4d48d",
+      _type: "image",
+      asset: {
+        _ref: "image-ee1472e0d1a893618cea925323cf0d4514d686c0-2048x2048-png",
+        _type: "reference",
+      },
+    },
+  ],
+  title: "7.6.22",
+};
+
+const five: Comic = {
+  _createdAt: "2022-07-06T18:06:07Z",
+  _id: "e09b8c7f-930a-4191-aab6-fd905fba8052",
+  _rev: "oV5AkreV0DW9Fuq9d9Yfwc",
+  _type: "comic",
+  _updatedAt: "2022-07-07T02:53:24Z",
+  description:
+    'Had a sudden bout of homesickness after eating an avocado. Also i misspelled "loss" to "los"... I miss LOS angeles . But only sometimes',
+  images: [
+    {
+      _key: "ca99e3e0e3f6",
+      _type: "image",
+      asset: {
+        _ref: "image-2d2d8be99aa621e537bea023b89993d2c5fe45e9-2048x2048-png",
+        _type: "reference",
+      },
+    },
+    {
+      _key: "a9c6d99d37f4",
+      _type: "image",
+      asset: {
+        _ref: "image-13c22f59f0d9ea7df09485f8f0d2e20e3064bbcd-2048x2048-png",
+        _type: "reference",
+      },
+    },
+    {
+      _key: "16b4bee4d48d",
+      _type: "image",
+      asset: {
+        _ref: "image-ee1472e0d1a893618cea925323cf0d4514d686c0-2048x2048-png",
+        _type: "reference",
+      },
+    },
+  ],
+  title: "7.6.22",
+};
+
+const sample: any[] = [
+  [],
+  [one],
+  [four, two, one, three],
+  [four, two, five, one, three],
+];
+
+describe("Comics Sorting Function", () => {
+  it("Returns same array if arr.length == 0 or 1", () => {
+    expect(comics_sorter(sample[0])).toEqual(sample[0]);
+    expect(comics_sorter(sample[1])).toEqual(sample[1]);
+    expect(comics_sorter([two])).toEqual([two]);
+    expect(comics_sorter([five])).toEqual([five]);
+  });
+
+  it("Returns a sorted group of comics by year, month, and then day", () => {
+    expect(comics_sorter(sample[2])).toEqual([one, two, three, four]);
+    expect(comics_sorter([three, four, two])).toEqual([two, three, four]);
+    expect(comics_sorter([five, one, two])).toEqual([one, two, five]);
+  });
+
+  it("Will sort identical title dates in place", () => {
+    expect(comics_sorter(sample[3])).toEqual([one, two, three, four, five]);
+    expect(comics_sorter([five, four, two, three, one])).toEqual([
+      one,
+      two,
+      three,
+      five,
+      four,
+    ]);
+  });
+});
