85cf595d87b82bcf3224d27d0ac78e963a8abdec
Xinrui Chen <xynree@gmail.com>
2022-07-16 21:36:24 -0700

    [UPDATE] Prev/Next does not show up when not possible, navigation styling on hover changed

--- components/ComicViewer/NavigationBtns/index.tsx
+++ components/ComicViewer/NavigationBtns/index.tsx
@@ -1,20 +1,24 @@
+import { Comic } from '../../../types/types'
+
+
 const {navigation, buttons} = {
   navigation: 'pl-4 pr-4 w-full flex gap-12 justify-between items-center font-nw  transition-all',
-  buttons: 'text-xs w-36 hover:[font-nw-thin] pt-2 pb-2 rounded-full hover:underline bg-[#FEFAEE]  transition-all'
+  buttons: 'text-xs w-36 hover:opacity-90 pt-2 pb-2 rounded-full  bg-[#FEFAEE] font-nw-bold  transition-all'
 }
 
 interface Props {
-  navigate: (() => void)[]
+  navigate: (() => void)[],
+  hasNavigate: [Comic|any,Comic|any]
 }
 
-const NavigationBtns = ({navigate}:Props) => {
+const NavigationBtns = ({navigate, hasNavigate}:Props) => {
   const [prev, home, next] = navigate
 
   return (
     <div className={navigation}>
-     <button className={buttons} onClick={prev}>prev</button>
+     <button style={{'visibility': hasNavigate[0]? 'visible':'hidden'}}className={buttons}  onClick={prev} >prev</button>
      <button className={buttons} onClick={home}>home</button>
-     <button className={buttons} onClick={next}>next</button>
+     <button style={{'visibility': hasNavigate[1]? 'visible':'hidden'}} className={buttons} onClick={next} >next</button>
     </div>
   );
 }
