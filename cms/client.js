3dc27b106f0968318d359e0c25d055a2990e3331
Xinrui Chen <xynree@gmail.com>
2022-07-18 11:05:43 -0700

    Added client id to env variables

--- cms/client.js
+++ cms/client.js
@@ -1,7 +1,7 @@
 import { createClient } from "next-sanity";
 
 export default createClient({
-  projectId: "y0nc1gqe",
+  projectId: process.env.NEXT_PUBLIC_CLIENT_ID,
   dataset: "production",
   apiVersion: "2022-06-16",
   useCdn: false,
